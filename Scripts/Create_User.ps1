6460c323b20d7513e5bf99d0e383dbec4adfebfd
CryptoDebug <axel10.lemaitre@gmail.com>
2025-06-06 14:19:06 +0200

    script to create a user

---
+++ Scripts/Create_User.ps1
@@ -0,0 +1,9 @@
+﻿New-ADUser `
+    -Name "By Powershell" `
+    -GivenName "By" `
+    -Surname "Powershell" `
+    -SamAccountName "bpowershell" `
+    -UserPrincipalName "bypowershell@ynovDomaine.com" `
+    -Path "OU=Utilisateurs,OU=Lille,OU=Nord,OU=France,OU=ynovGroup,DC=ynovDomaine,DC=com" `
+    -AccountPassword (ConvertTo-SecureString "Powershell!" -AsPlainText -Force) `
+    -Enabled $true
