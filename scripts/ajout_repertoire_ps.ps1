a64878f47f65accee5940d0a35109db948fb8e06
lechatn <noe.lechat@ynov.com>
2025-06-10 15:05:51 +0200

    add scripts and csv files from last edit

---
+++ scripts/ajout_repertoire_ps.ps1
@@ -0,0 +1,23 @@
+﻿New-Item -Path "C:\Shares\ps_Repertoire_1" -ItemType Directory
+New-Item -Path "C:\Shares\ps_Repertoire_2" -ItemType Directory
+New-Item -Path "C:\Shares\ps_Repertoire_3" -ItemType Directory
+New-Item -Path "C:\Shares\ps_Repertoire_4" -ItemType Directory
+New-Item -Path "C:\Shares\ps_Repertoire_5" -ItemType Directory
+New-Item -Path "C:\Shares\ps_Repertoire_6" -ItemType Directory
+New-Item -Path "C:\Shares\ps_Repertoire_7" -ItemType Directory
+New-Item -Path "C:\Shares\ps_Repertoire_8" -ItemType Directory
+New-Item -Path "C:\Shares\ps_Repertoire_9" -ItemType Directory
+New-Item -Path "C:\Shares\ps_Repertoire_10" -ItemType Directory
+New-Item -Path "C:\Shares\ps_Repertoire_11" -ItemType Directory
+New-Item -Path "C:\Shares\ps_Repertoire_12" -ItemType Directory
+New-Item -Path "C:\Shares\ps_Repertoire_13" -ItemType Directory
+New-Item -Path "C:\Shares\ps_Repertoire_14" -ItemType Directory
+New-Item -Path "C:\Shares\ps_Repertoire_15" -ItemType Directory
+New-Item -Path "C:\Shares\ps_Repertoire_16" -ItemType Directory
+New-Item -Path "C:\Shares\ps_Repertoire_17" -ItemType Directory
+New-Item -Path "C:\Shares\ps_Repertoire_18" -ItemType Directory
+New-Item -Path "C:\Shares\ps_Repertoire_19" -ItemType Directory
+New-Item -Path "C:\Shares\ps_Repertoire_20" -ItemType Directory
+
+New-SmbShare -Name "ps_Repertoire_1" -Path "C:\Shares\ps_Repertoire_1" -ReadAccess "YnovNL\Direction,YnovNL\Communication,YnovNL\Service_sociale,YnovNL\Service_enseignant" -ChangeAccess "YnovNL\Technique,YnovNL\Service_Salle" -FullAccess "YnovNL\Commercial,YnovNL\Service_Sport"
+
---
+++ scripts/ajout_utilisateur_ps.ps1
@@ -0,0 +1,98 @@
+﻿New-ADUser `
+    -Name "Martin Pierre" `
+    -GivenName "Pierre" `
+    -Surname "Martin" `
+    -SamAccountName "pmartin" `
+    -UserPrincipalName "pierre.martin@ynovNL.com" `
+    -Path "OU=Utilisateurs,OU=Paris-Ouest,OU=Paris,OU=france,OU=ynovGroup,DC=YnovNL,DC=com" `
+    -AccountPassword (ConvertTo-SecureString "#root1234" -AsPlainText -Force) `
+    -Enabled $true
+
+
+
+New-ADUser `
+    -Name "Blond Camille" `
+    -GivenName "Camille" `
+    -Surname "Blond" `
+    -SamAccountName "cblond" `
+    -UserPrincipalName "camille.blond@ynovNL.com" `
+    -Path "OU=Utilisateurs,OU=Lyon,OU=Est,OU=france,OU=ynovGroup,DC=YnovNL,DC=com" `
+    -AccountPassword (ConvertTo-SecureString "#root1234" -AsPlainText -Force) `
+    -Enabled $true
+
+
+New-ADUser `
+    -Name "Lelong Jean" `
+    -GivenName "Jean" `
+    -Surname "Lelong" `
+    -SamAccountName "jlelong" `
+    -UserPrincipalName "jean.lelong@ynovNL.com" `
+    -Path "OU=Utilisateurs,OU=Aix-en-Provence,OU=Sud,OU=france,OU=ynovGroup,DC=YnovNL,DC=com" `
+    -AccountPassword (ConvertTo-SecureString "#root1234" -AsPlainText -Force) `
+    -Enabled $true
+
+
+New-ADUser `
+    -Name "Metayer Julie" `
+    -GivenName "Julie" `
+    -Surname "Metayer" `
+    -SamAccountName "jmetayer" `
+    -UserPrincipalName "julie.metayer@ynovNL.com" `
+    -Path "OU=Utilisateurs,OU=Lille,OU=Nord,OU=france,OU=ynovGroup,DC=YnovNL,DC=com" `
+    -AccountPassword (ConvertTo-SecureString "#root1234" -AsPlainText -Force) `
+    -Enabled $true
+
+
+New-ADUser `
+    -Name "Fort Axel" `
+    -GivenName "Axel" `
+    -Surname "Fort" `
+    -SamAccountName "afort" `
+    -UserPrincipalName "axel.fort@ynovNL.com" `
+    -Path "OU=Utilisateurs,OU=Casablanca,OU=internantional,OU=ynovGroup,DC=YnovNL,DC=com" `
+    -AccountPassword (ConvertTo-SecureString "#root1234" -AsPlainText -Force) `
+    -Enabled $true
+
+
+New-ADUser `
+    -Name "Leduc Etienne" `
+    -GivenName "Etienne" `
+    -Surname "Leduc" `
+    -SamAccountName "eleduc" `
+    -UserPrincipalName "etienne.leduc@ynovNL.com" `
+    -Path "OU=Utilisateurs,OU=Casablanca,OU=internantional,OU=ynovGroup,DC=YnovNL,DC=com" `
+    -AccountPassword (ConvertTo-SecureString "#root1234" -AsPlainText -Force) `
+    -Enabled $true
+
+
+New-ADUser `
+    -Name "Boissier Rose" `
+    -GivenName "Rose" `
+    -Surname "Boissier" `
+    -SamAccountName "rboissier" `
+    -UserPrincipalName "rose.boissier@ynovNL.com" `
+    -Path "OU=Utilisateurs,OU=Paris-Ouest,OU=Paris,OU=france,OU=ynovGroup,DC=YnovNL,DC=com" `
+    -AccountPassword (ConvertTo-SecureString "#root1234" -AsPlainText -Force) `
+    -Enabled $true
+
+
+New-ADUser `
+    -Name "Klein Johan" `
+    -GivenName "Johan" `
+    -Surname "Klein" `
+    -SamAccountName "jklein" `
+    -UserPrincipalName "johan.klein@ynovNL.com" `
+    -Path "OU=Utilisateurs,OU=Nice-Sophia,OU=Sud,OU=france,OU=ynovGroup,DC=YnovNL,DC=com" `
+    -AccountPassword (ConvertTo-SecureString "#root1234" -AsPlainText -Force) `
+    -Enabled $true
+
+
+New-ADUser `
+    -Name "Bouchet Xavier" `
+    -GivenName "Xavier" `
+    -Surname "Bouchet" `
+    -SamAccountName "xbouchet" `
+    -UserPrincipalName "xavier.bouchet@ynovNL.com" `
+    -Path "OU=Utilisateurs,OU=Lille,OU=Nord,OU=france,OU=ynovGroup,DC=YnovNL,DC=com" `
+    -AccountPassword (ConvertTo-SecureString "#root1234" -AsPlainText -Force) `
+    -Enabled $true
